383d85b8a114cb3cba4e0fb4844ae3cc599ec25d
MarcosTort <72978798+MarcosTort@users.noreply.github.com>
2021-04-11 15:10:45 -0300

    Update cadena.cpp

--- tarea2/src/cadena.cpp
+++ tarea2/src/cadena.cpp
@@ -149,40 +149,22 @@ TCadena insertarAntes(TInfo i, TLocalizador loc, TCadena cad) {
 }
 
 TCadena removerDeCadena(TLocalizador loc, TCadena cad) {
-  assert(localizadorEnCadena(loc, cad));
-  TLocalizador aux = loc;
-    if(localizadorEnCadena(loc, cad)){
-
-	if (esInicioCadena(loc, cad)) {
-		//inicio
-		if (esLocalizador(loc->siguiente)) {
-      cad->inicio = aux->siguiente; 
-			aux->siguiente->anterior = NULL; 
-			liberarInfo(loc->dato);
-			delete loc;
-		} else {
-      cad->inicio = NULL;
-			cad->final = NULL;
-			liberarInfo(loc->dato);
-			delete loc;
-		}
-
-	} else {
-		if (!esFinalCadena(loc, cad)) {
-       //medio 
-			aux->anterior->siguiente = aux->siguiente;
-			aux->siguiente->anterior = aux->anterior;
-			liberarInfo(loc->dato);
-			delete loc;
-		} else {
-      //final 
-			cad->final = aux->anterior;
-			aux->anterior->siguiente = NULL;
-			liberarInfo(loc->dato);
-			delete loc;
-		}
-	}
+  if (!esVaciaCadena(cad)) {
+    liberarInfo(loc->dato);
+    if ((esInicioCadena(loc, cad)) && (esFinalCadena(loc, cad))) {
+      cad->inicio = cad->final = NULL;
+    } else if (esInicioCadena(loc, cad)) {
+      cad->inicio = loc->siguiente;
+      cad->inicio->anterior = NULL;
+    } else if (esFinalCadena(loc, cad)) {
+      cad->final = loc->anterior;
+      cad->final->siguiente = NULL;
+    } else {
+      loc->anterior->siguiente = loc->siguiente;
+      loc->siguiente->anterior = loc->anterior;
     }
+    delete loc;
+  }
 return cad;
 }
 
