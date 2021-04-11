b47630a79f476d41a593c5548c4698dbb969e9ed
MarcosTort <72978798+MarcosTort@users.noreply.github.com>
2021-04-11 11:29:21 -0300

    Update cadena.cpp

--- tarea2/src/cadena.cpp
+++ tarea2/src/cadena.cpp
@@ -192,15 +192,15 @@ void imprimirCadena(TCadena cad) {
     if(l->siguiente != NULL){
       
       while (l->siguiente != NULL){
-        char *dat = infoATexto(l->dato);
-        printf(dat);
+        char[] *dat = infoATexto(l->dato);
+        printf("%s", dat);
         l = l->siguiente;
         delete[] dat;
       }//end while
     }//end segundo if
     else{
-      char *dat2 = infoATexto(l->dato); //imprime unico dato
-      printf(dat2);
+      char[] *dat2 = infoATexto(l->dato); //imprime unico dato
+      printf("%s", dat2);
       delete[] dat2;
     }//end segundo else
   }
